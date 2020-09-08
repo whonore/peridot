60e44f484238818667c8b9b57a057c542746d4a6
whonore <wolfhonore@gmail.com>
2020-09-08 17:10:24 -0400

    Create missing symlinks

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,79 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::env;
 use std::fmt;
 use std::fs;
+use std::io;
+use std::os::unix;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
+type Apps = HashMap<String, AppConfig>;
+
 #[derive(Debug, Deserialize)]
 #[serde(transparent)]
-struct Config(HashMap<String, AppConfig>);
+struct AppsWrap(Apps);
 
-type Link = (String, String);
+type LinkPair = (String, String);
 
 #[derive(Debug, Deserialize)]
 struct AppConfig {
     dir: Option<String>,
     description: Option<String>,
-    links: Option<Vec<Link>>,
+    links: Option<Vec<LinkPair>>,
 }
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "dotty", about = "A dotfile manager")]
 struct Cli {
     #[structopt(parse(from_os_str))]
-    dotfile_dir: Option<PathBuf>,
+    base_dir: Option<PathBuf>,
     #[structopt(short = "c", long = "config-file", parse(from_os_str))]
     config_file: Option<PathBuf>,
+    #[structopt(short = "C", long = "check-only")]
+    check_only: bool,
 }
 
 fn home_dir() -> PathBuf {
     PathBuf::from(env::var("HOME").unwrap())
 }
 
-fn find_config(dotfiles: &Path) -> PathBuf {
-    dotfiles.join("dotty.toml")
+fn find_config(base_dir: &Path) -> PathBuf {
+    base_dir.join("dotty.toml")
 }
 
-fn parse_config(args: Cli) -> Result<(PathBuf, Config)> {
-    let dotfile_dir = args
-        .dotfile_dir
-        .unwrap_or_else(|| home_dir().join(".dotfiles"))
-        .canonicalize()?;
-    let config_file = args
-        .config_file
-        .unwrap_or_else(|| find_config(&dotfile_dir))
-        .canonicalize()?;
-    let config: Config = toml::from_str(&std::fs::read_to_string(&config_file)?)?;
-
-    Ok((dotfile_dir, config))
+#[derive(Debug)]
+struct Config {
+    base_dir: PathBuf,
+    apps: Apps,
+    check_only: bool,
 }
 
-fn to_path(path: &str) -> Result<PathBuf> {
-    Path::new(path)
-        .iter()
-        .map(|comp| {
-            let comp = comp
-                .to_str()
-                .ok_or_else(|| anyhow!("Can't convert {} to a str", comp.to_string_lossy()))?;
-            if comp.starts_with('$') {
-                env::var(&comp[1..]).or_else(|_| Err(anyhow!("Can't find {}", comp)))
-            } else {
-                Ok(comp.into())
-            }
+impl Config {
+    fn new(args: Cli) -> Result<Config> {
+        let base_dir = args
+            .base_dir
+            .unwrap_or_else(|| home_dir().join(".dotfiles"))
+            .canonicalize()?;
+        let config_file = args
+            .config_file
+            .unwrap_or_else(|| find_config(&base_dir))
+            .canonicalize()?;
+        let apps: AppsWrap = toml::from_str(&std::fs::read_to_string(&config_file)?)?;
+
+        Ok(Config {
+            base_dir,
+            apps: apps.0,
+            check_only: args.check_only,
         })
-        .collect()
+    }
 }
 
 #[derive(Debug)]
 struct AppOutput {
     name: String,
+    // TODO: use Ok, Fail, ? type and push formatting to Display
     results: Vec<String>,
 }
 
@@ -81,52 +85,202 @@ impl AppOutput {
         }
     }
 
-    fn add_result(&mut self, res: String) {
-        self.results.push(res)
+    fn add_link(&mut self, link: &Link) {
+        self.results.push(match &link.status {
+            SrcUnexists => format!("?─ {} ↛ {}", link.src.display(), link.dst.display()),
+            DstUnexists => format!(
+                "❌─ {} ↛ {} (Failed: target does not exist)",
+                link.src.display(),
+                link.dst.display()
+            ),
+            Exists => format!("✓─ {} → {}", link.src.display(), link.dst.display()),
+            Unexpected(found) => format!(
+                "❌─ {} ↛ {} (expected: {})",
+                link.src.display(),
+                found.display(),
+                link.dst.display()
+            ),
+        })
+    }
+
+    fn add_error(&mut self, error: &PathError, src: Option<&PathBuf>, dst: Option<&PathBuf>) {
+        if src.is_none() || dst.is_none() {
+            self.results.push(format!("❌─ (Failed: {})", error))
+        } else {
+            self.results.push(format!(
+                "❌─ {} ↛ {} (Failed: {})",
+                src.unwrap().display(),
+                dst.unwrap().display(),
+                error
+            ))
+        }
     }
 }
 
 impl fmt::Display for AppOutput {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: put name in a box
         write!(f, "{}", self.name)?;
         if let Some((last, results)) = self.results.split_last() {
             results
                 .iter()
-                .map(|res| write!(f, "\n├── {}", res))
+                .map(|res| write!(f, "\n├──{}", res))
                 .collect::<fmt::Result>()?;
-            write!(f, "\n└── {}", last)?;
+            write!(f, "\n└──{}", last)?;
         }
         writeln!(f)
     }
 }
 
-fn check_link(out: &mut AppOutput, dir: &PathBuf, link: &Link) -> Result<()> {
-    let (to, from) = link;
-    let to = dir.join(to_path(to)?);
-    let from = to_path(from)?;
-    let real_to = fs::read_link(&from)?;
-
-    if to == real_to {
-        out.add_result(format!("✓  {} → {}", from.display(), to.display()))
-    } else {
-        out.add_result(format!(
-            "❌  {} → {} (expected {})",
-            from.display(),
-            real_to.display(),
-            to.display()
-        ))
-    };
-    Ok(())
+#[derive(Debug)]
+enum PathError {
+    InvalidEnvVar { path: String, env: String },
+    NoParent(String),
+    IoError(io::Error),
 }
 
-fn check(dotfile: &PathBuf, name: &str, app: &AppConfig) -> Result<()> {
+impl fmt::Display for PathError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            InvalidEnvVar { path, env } => {
+                write!(f, "Could not find environment variable {} in {}", env, path)
+            }
+            NoParent(path) => write!(f, "{} must have a parent directory", path),
+            IoError(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl From<io::Error> for PathError {
+    fn from(e: io::Error) -> Self {
+        IoError(e)
+    }
+}
+
+#[derive(Debug)]
+enum LinkStatus {
+    SrcUnexists,
+    DstUnexists,
+    Exists,
+    Unexpected(PathBuf),
+}
+
+#[derive(Debug)]
+struct Link {
+    src: PathBuf,
+    dst: PathBuf,
+    status: LinkStatus,
+}
+
+impl Link {
+    fn src_unexists(src: PathBuf, dst: PathBuf) -> Self {
+        Link {
+            src,
+            dst,
+            status: SrcUnexists,
+        }
+    }
+
+    fn dst_unexists(src: PathBuf, dst: PathBuf) -> Self {
+        Link {
+            src,
+            dst,
+            status: DstUnexists,
+        }
+    }
+
+    fn exists(src: PathBuf, dst: PathBuf) -> Self {
+        Link {
+            src,
+            dst,
+            status: Exists,
+        }
+    }
+
+    fn unexpected(src: PathBuf, dst: PathBuf, expected: PathBuf) -> Self {
+        Link {
+            src,
+            dst,
+            status: Unexpected(expected),
+        }
+    }
+}
+
+use LinkStatus::*;
+use PathError::*;
+
+fn to_path(path: &str) -> std::result::Result<PathBuf, PathError> {
+    Path::new(path)
+        .iter()
+        .map(|comp| {
+            let comp = comp.to_str().unwrap();
+            if comp.starts_with('$') {
+                env::var(&comp[1..]).or_else(|_| {
+                    Err(InvalidEnvVar {
+                        path: path.into(),
+                        env: comp.into(),
+                    })
+                })
+            } else {
+                Ok(comp.into())
+            }
+        })
+        .collect()
+}
+
+fn check_link(dir: &PathBuf, link: &LinkPair) -> std::result::Result<Link, PathError> {
+    let (dst, src) = link;
+    let dst = dir.join(to_path(dst)?);
+    let src = to_path(src)?;
+
+    if src.exists() {
+        let real_dst = src.read_link()?;
+        if dst == real_dst {
+            Ok(Link::exists(src, dst))
+        } else {
+            Ok(Link::unexpected(src, dst, real_dst))
+        }
+    } else if !dst.exists() {
+        Ok(Link::dst_unexists(src, dst))
+    } else {
+        Ok(Link::src_unexists(src, dst))
+    }
+}
+
+fn make_link(src: &PathBuf, dst: &PathBuf) -> std::result::Result<Link, PathError> {
+    let dir = src
+        .parent()
+        .ok_or_else(|| NoParent(src.display().to_string()))?;
+    fs::create_dir_all(dir)?;
+    unix::fs::symlink(dst, src)?;
+    Ok(Link::exists(src.clone(), dst.clone()))
+}
+
+fn link(base_dir: &PathBuf, name: &str, app: &AppConfig, check_only: bool) -> Result<()> {
     let mut out = AppOutput::new(name);
-    let dir = dotfile
+    let dir = base_dir
         .join(app.dir.as_deref().unwrap_or(name))
         .canonicalize()?;
     if let Some(links) = &app.links {
         for link in links {
-            check_link(&mut out, &dir, link)?
+            match check_link(&dir, link) {
+                Ok(link) => match link.status {
+                    SrcUnexists => {
+                        if !check_only {
+                            match make_link(&link.src, &link.dst) {
+                                Ok(link) => out.add_link(&link),
+                                Err(e) => out.add_error(&e, Some(&link.src), Some(&link.dst)),
+                            }
+                        } else {
+                            out.add_link(&link)
+                        }
+                    }
+                    DstUnexists => out.add_link(&link),
+                    Exists => out.add_link(&link),
+                    Unexpected(_) => out.add_link(&link),
+                },
+                Err(e) => out.add_error(&e, None, None),
+            }
         }
     };
     println!("{}", out);
@@ -135,11 +289,11 @@ fn check(dotfile: &PathBuf, name: &str, app: &AppConfig) -> Result<()> {
 
 fn main() -> Result<()> {
     let args = Cli::from_args();
-    let (dotfile_dir, config) = parse_config(args)?;
+    let config = Config::new(args)?;
 
     config
-        .0
+        .apps
         .iter()
-        .map(|(name, app)| check(&dotfile_dir, name, app))
+        .map(|(name, app)| link(&config.base_dir, name, app, config.check_only))
         .collect()
 }
