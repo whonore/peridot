f90bfce06cfc5eb7396d5de4f320a3e97dc920dc
whonore <wolfhonore@gmail.com>
2022-03-25 13:50:54 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,21 +3,24 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      defaultPackage.x86_64-linux = pkgs.rustPlatform.buildRustPackage {
-        pname = "peridot";
-        version = "0.1.1";
-        src = ./.;
-        cargoSha256 = "0dkyja8i7dkxn1pr9xpxlxfwx0sffn6xnwv9nylrscapvqmz68vf";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "peridot";
+      version = "0.1.1";
+      src = ./.;
+      cargoSha256 = "0dkyja8i7dkxn1pr9xpxlxfwx0sffn6xnwv9nylrscapvqmz68vf";
 
-        meta = with pkgs.lib; {
-          description = "A dotfile manager";
-          homepage = "https://github.com/whonore/peridot";
-          license = with licenses; [ mit ];
-          maintainers = with maintainers; [ whonore ];
-        };
+      meta = with pkgs.lib; {
+        description = "A dotfile manager";
+        homepage = "https://github.com/whonore/peridot";
+        license = with licenses; [mit];
+        maintainers = with maintainers; [whonore];
       };
     };
+  };
 }
